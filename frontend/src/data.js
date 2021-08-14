67275bd3a64e69bbd75fe4d151f4e17046f9d7f8
Moses Ojiko <mosesojiko999@gmail.com>
2021-08-14 08:48:26 -0700

    Translate html to react app

---
+++ frontend/src/data.js
@@ -0,0 +1,73 @@
+
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: "Native Atire",
+            category: "Shirts",
+            image: '/images/product-1.jpg',
+            price: 120,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            dsecription: "High quality product."
+        },
+        {
+            _id: '2',
+            name: "Native Atire 2",
+            category: "Shirts",
+            image: '/images/product-2.jpg',
+            price: 200,
+            brand: "Nike 2",
+            rating: 4.0,
+            numReviews: 8,
+            dsecription: "High quality product."
+        },
+        {
+            _id: '6',
+            name: "Native Atire 3",
+            category: "Shirts",
+            image: '/images/product-6.jpg',
+            price: 150,
+            brand: "Nike",
+            rating: 4.8,
+            numReviews: 11,
+            dsecription: "High quality product."
+        },
+        {
+            _id: '4',
+            name: "Native Atire 4",
+            category: "Shirts",
+            image: '/images/product-4.jpg',
+            price: 300,
+            brand: "Nike",
+            rating: 4.9,
+            numReviews: 27,
+            dsecription: "High quality product."
+        },
+        {
+            _id: '5',
+            name: "Native Atire 5",
+            category: "Shirts",
+            image: '/images/product-5.jpg',
+            price: 240,
+            brand: "Nike",
+            rating: 5.0,
+            numReviews: 34,
+            dsecription: "High quality product."
+        },
+        {
+            _id: '6',
+            name: "Native Atire 6",
+            category: "Shirts",
+            image: '/images/product-6.jpg',
+            price: 150,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 7,
+            dsecription: "High quality product."
+        },
+    ]
+}
+
+export default data;
---
+++ readme.md
@@ -0,0 +1,11 @@
+1. Create a react app
+2. Add product attributes
+3. Add link, images, name and prices
+
+# Create react app
+1. npx create-react-app frontend
+2. npm start
+3. Remove unused files
+4. Copy index.html content to App.js
+5. Copy style.css to index.css
+6. Replace class with className
