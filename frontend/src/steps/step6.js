935c7340ba0287262a2bdf49d693b3924530166f
Moses Ojiko <mosesojiko999@gmail.com>
2021-08-18 10:58:43 -0700

    Feat: eslint, .env for frontend

---
+++ frontend/src/steps/step6.js
@@ -0,0 +1,11 @@
+/* 
+Install ESlint for code linting
+
+1. install vsCode eslint extension
+2. npm install -D eslint
+3. run ./node_modules/.bin/eslint --init
+4. create ./frontend/.env
+5. Add SKIP_PREFLIGHT_CHECK=true
+ */
+
+//to disable eslint use ctrl + shift + p, and type eslint, select disable
